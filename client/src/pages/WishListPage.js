d4533087f001197a73df96e09b3fe2cf64f73158
Aydın Kurt <aydin@Aydn-MacBook-Pro.local>
2025-05-06 19:44:47 +0300

    Start writing Wish List Page.

---
+++ client/src/pages/WishListPage.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NavigationBar from './HomePage/NavigationBar';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
