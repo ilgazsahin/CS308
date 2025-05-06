4fc4c113ae9fd3f230cecaeb5ba032ad495dbe15
Ahmed Berka Ayhun <berka.ayhun@sabanciuniv.edu>
2025-05-06 18:13:04 +0300

    Add route to add items to wishlist

--- Controller/WishListController.js
+++ Controller/WishListController.js
@@ -2,3 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Wishlist = require('../Models/WishListModel');
 
+router.post('/', async (req, res) => {
+  const { userId, bookId } = req.body;
+
+  try {
+    const exists = await Wishlist.findOne({ userId, bookId });
+    if (exists) {
+      return res.status(409).json({ message: 'Already in wishlist' });
+    }
+
+    const newItem = new Wishlist({ userId, bookId });
+    await newItem.save();
+
+    res.status(201).json({ message: 'Added to wishlist', item: newItem });
+  } catch (err) {
+    res.status(500).json({ message: 'Error adding to wishlist', error: err });
+  }
+});
+
+
+
+module.exports = router;
