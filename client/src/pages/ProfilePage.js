ccaecf6a67454e150adc2df099f0989bdf92a7a6
TUANA <tuanab@TUANA-MacBook-Pro.local>
2025-05-13 15:31:47 +0300

    Start Profile Page

---
+++ client/src/pages/ProfilePage.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import NavigationBar from './HomePage/NavigationBar';
+import Footer from '../components/Footer';
+
+const ProfilePage = () => {
+}
