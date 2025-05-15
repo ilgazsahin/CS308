3b7ff78a09a525219603731ec49e3de7a5aa30c5
Ahmed Berka Ayhun <berka.ayhun@sabanciuniv.edu>
2025-05-15 22:03:05 +0300

    Add new routes

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -5,7 +5,7 @@ import MainPage from './pages/HomePage/HomePage';
 import Signup from './pages/Signup';
 import AddBook from "./pages/AddBook";
 import BookDetail from "./pages/BookDetail";
-import Dashboard from "./pages/AdminDashboard";
+import Dashboard from "./pages/ProductManagerPage";
 import EditBook from "./pages/EditBook";  // Make sure you import your EditBook component
 import ProductsPage from "./pages/ProductsPage";
 import AboutPage from "./pages/AboutPage";
@@ -16,7 +16,17 @@ import InvoicePage from "./pages/InvoicePage";
 import StockManagement from "./pages/StockManagement"; // Import the new StockManagement page
 import { CartProvider } from './components/CartContext';
 import ManageOrders from './pages/ManageOrders';
-
+import WishlistPage from './pages/WishListPage';
+import ProfilePage from './pages/ProfilePage';
+import RefundRequest from './pages/RefundRequest';
+import ManageRefunds from './pages/ManageRefunds';
+import UserRefund from './pages/UserRefund';
+import SalesManagerDashboard from './pages/SalesManagerDashboard';
+import InvoiceView from './pages/InvoiceVİew';
+import InvoiceDetail from './pages/InvoiceDetailPage';
+import CommentApproval from './pages/CommentApprovalPage';
+import CommentManagementPage from './pages/CommentManagementPage';
+import ProfitReport from './pages/ProfitReport';
 const Navigation = () => {
     return (
         <Router>
@@ -47,10 +57,31 @@ const Navigation = () => {
                     <Route path="/admin" element={<Dashboard />} />
                     <Route path="/admin/stock" element={<StockManagement />} />
                     <Route path="/admin/manage-orders" element={<ManageOrders />} />
+                    <Route path="/admin/invoices" element={<InvoiceView />} />
+                    <Route path="/admin/detailinvoice/:invoiceId" element={<InvoiceDetail/>} />
+                    <Route path="/admin/approval" element={<CommentApproval/>} />
+                    <Route path="/admin/commentmanagement" element={<CommentManagementPage/>} />
+
+
                     {/* Wish List */}
                     <Route path="/wishlist" element={<WishlistPage />} />
                     {/* User Profile */}
                     <Route path="/profile" element={<ProfilePage />} />
+                    {/* Refund Pages */}
+
+                    <Route path="/refund-request/:orderId" element={<RefundRequest />} />
+                    <Route path="/refunds" element={<UserRefund />} />
+                    {/*Sales Manager  */}
+                    <Route path="/sales-manager" element={<SalesManagerDashboard/>} />
+                    <Route path="/sales-manager/manage-refunds" element={<ManageRefunds />} />
+                    <Route path="/sales-manager/profit-report" element={<ProfitReport />} />
+
+
+
+
+
+
+
                 </Routes>
             </CartProvider>
         </Router>
