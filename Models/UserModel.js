a5616c2d276e217b516055ee26bfe2f7a3bed321
Ahmed Berka Ayhun <berka.ayhun@sabanciuniv.edu>
2025-05-15 22:21:35 +0300

    Add roles and change users to User

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    password: String,
+    address: String,
+    role: {
+        type: String,
+        enum: ["customer", "product manager", "sales manager"],
+        default: "customer"
+  }
 })
 
-const UserModel = mongoose.model("users", UserSchema)
+const UserModel = mongoose.model("User", UserSchema)
 module.exports = UserModel
     
